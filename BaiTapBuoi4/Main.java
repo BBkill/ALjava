29f76c4e69d54dea3756d3aeb9b858d1a4abec9e
BBkill <dominhduc12022001@gmail.com>
2021-09-29 15:48:27 +0700

    Bài tập buổi 4

---
+++ BaiTapBuoi4/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start tming");
+        Timer timer = new Timer(0, 0, 0);
+        timer.start();
+    }
+}
